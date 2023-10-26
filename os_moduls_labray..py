9d6af8ed3d58cfc77cf58aaecf024c36758b67cc
Ravi Kumawat <145572488+1998rv@users.noreply.github.com>
2023-10-26 16:55:42 +0530

    #library#python#pycham#moduls

---
+++ os_moduls_labray..py
@@ -0,0 +1,15 @@
+import os # OS Module in Python
+
+#   os.getcwd() # Handling the Current Working Directory
+#   os.chdir()  # Changing the Current working directory
+#   os.listdir() # prsent the code path in a list all the file []
+#   os.mkdir()   # mkdir make a new file in current Working Directory
+#   os.makedirs() # makedirs make a new file in out off present working Directory
+#    os.remove()  # remove directory remove file which we remove us using the path
+#   os.rmdir()    # remove rendam a file in current path
+#    os.name()     # this method make file name
+#   os.rename()    # rename the file
+#   os.close()     # close the current run file
+
+
+# most use directory or useable directoru
